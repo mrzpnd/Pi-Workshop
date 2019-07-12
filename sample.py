6472babe46caf80bda9769092cdb16bd8a55112f
mrzpnd <mrzpnd@gmail.com>
2019-07-12 07:25:36 +0545

    codes

---
+++ sample.py
@@ -0,0 +1,60 @@
+test = "sample {}".format("program")
+print(test)
+test1="sample"+"program"
+print(test1)
+test2="{} program".format(1)
+print (test2)
+test3=str(1)+"program"
+print(test3)
+name= "my name is {} and im an {}".format("miraz","engineer")
+print(name) 
+faculty_batch="I am from {faculty} and {batch}".format(batch="2016",faculty="electronics and communication")
+print(faculty_batch) 
+
+num=10
+print(num)
+num1=10.5
+print(num1)
+num2=10/3
+print(num2)
+num4=10.0+2
+print(num4)
+print(10/3)
+print(2**3)
+#conditional statements
+x=10 
+if x<10:
+    print("The number is less than 10")
+else:
+    print("The number is greater than 10")
+
+
+
+
+y=11
+
+if y%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+
+
+
+
+z=-0
+if z==0:
+    print("the number is equal to zero")
+elif z<0:
+    print("the number is less than zero")
+else :
+    print("the number is greater than zero")
+
+z=int(input("enter any number"))
+
+if z==0:
+    print("the number is equal to zero")
+elif z<0:
+    print("the number is negative")
+else :
+    print("the number 
